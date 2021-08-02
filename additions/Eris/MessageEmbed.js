cbc8a54b91ed091e5d7ad97c0f19003503caa033
Exhabition <lukeweerheim@hotmail.com>
2021-08-03 00:30:33 +0200

    Most of the basic setters and logic for MessageEmbed

--- additions/Eris/MessageEmbed.js
+++ additions/Eris/MessageEmbed.js
@@ -1,82 +1,209 @@
+const { addImport } = require("erisify/helper/prototype");
+const { truncate } = require("erisify/helper/formatting");
+
+const { preventErrors } = require("erisify/index.js").options;
+
+/** TODO:
+ * Turn values into strings when possible.
+ * Stricter checking for HEX strings is
+ * Take a look at `setTimestamp` again
+ * Check the types of values, a boolean parameter shouldn't receive a number for example
+*/
+
 class MessageEmbed {
-    constructor() {
+    /**
+     * @param {Object} options
+     * @param {Boolean} [options.preventErrors] [Overrides initial options of erisify] Try and fix errors such as making strings shorter instead of throwing an error if the string is too long for this specific embed
+     * @param {String} [options.truncate] If a present values that are too long will get truncated and will end into the string passed in this option
+    */
+    constructor(options = { preventErrors }) {
+        this.options = options;
         this.fields = [];
 
         return this;
     }
 
-    setAuthor(name, icon_url, url) {
-        this.author = { name, icon_url, url };
+    /**
+     * @param {String} name
+     * @param {String} [iconUrl]
+     * @param {String} [url]
+     */
+    setAuthor(name, iconUrl, url) {
+        const tempAuthor = {};
+
+        if (this.options.truncate || this.options.preventErrors) {
+            const suffix = this.options.truncate || "";
+            tempAuthor.name = truncate(name, 256, suffix);
+            if (iconUrl) tempAuthor.icon_url = truncate(iconUrl, 2048, suffix);
+            if (url) tempAuthor.url = truncate(url, 2048, suffix);
+        } else {
+            if (name.length > 0 && name.length <= 256) tempAuthor.name = name;
+            else throw new RangeError("[MessageEmbed.setAuthor] 'name' must be a string longer than 0 characters and shorter than or equal to 256 characters");
+
+            if (iconUrl) {
+                if (iconUrl.length > 0 && iconUrl.length <= 2048) tempAuthor.icon_url = iconUrl;
+                else throw new RangeError("[MessageEmbed.setAuthor] 'iconUrl' must be a string longer than 0 characters and shorter than or equal to 2048 characters");
+            }
+
+            if (url) {
+                if (url.length > 0 && url.length <= 2048) tempAuthor.url = url;
+                else throw new RangeError("[MessageEmbed.setAuthor] 'url' must be a string longer than 0 characters and shorter than or equal to 2048 characters");
+            }
+        }
+
+        this.author = tempAuthor;
 
         return this;
     }
 
+    /**
+     * @param {Number|String} color Either a HEX string or number
+     */
     setColor(color) {
-        if (color.startsWith("#")) this.color = parseInt(color, 16);
-        else this.color = color;
+        if (typeof color == "number") {
+            if (color > 0 && color < 16777215) this.color = color;
+            else throw new RangeError("[MessageEmbed.setColor] 'color' must be a number higher than 0 and lower than or equal to 16777216777215");
+        } else if (color.startsWith("#")) {
+            this.color = parseInt(color.slice(1, color.length), 16);
+        } else {
+            throw new Error("[MessageEmbed.setColor] Invalid HEX string passed as 'color'");
+        }
 
         return this;
     }
 
+    /**
+     * @param {String} title Prefers a string, but numbers or arrays are turned into strings too
+     */
     setTitle(title) {
-        this.title = title.toString().slice(256);
+        if (this.options.truncate) this.title = truncate(title, 256, this.options.truncate);
+        else if (this.options.preventErrors) his.title = truncate(title, 256);
+        else if (title.length > 0 && title.length <= 256) this.title = title;
+        else throw new RangeError("[MessageEmbed.setTitle] 'title' must be a string longer than 0 and shorter or equal to 256 characters");
 
         return this;
     }
 
-    setUrl(url) {
-        this.url = url;
+    /**
+     * @param {String} url
+     */
+    setURL(url) {
+        // No clue why url limits are not documented...
+        if (url.length > 0 && url.length <= 2048) this.url = url;
+        else throw new RangeError("[MessageEmbed.setURL] 'url' must be a well-formed URL longer than 0 and shorter or equal to 2048 characters");
 
         return this;
     }
 
+    /**
+     * @param {String} description
+     */
     setDescription(description) {
-        this.description = description.toString().slice(2048);
+        if (this.options.truncate || this.options.preventErrors) this.description = truncate(description.toString(), 4096, this.options.truncate || "");
+        else if (description.length > 0 && description.length <= 4096) this.description = description;
+        else throw new RangeError("[MessageEmbed.setDescription] 'description' must be a string longer than 0 and shorter or equal to 4096 characters");
 
         return this;
     }
 
+    /**
+     * @param {String} url An url pointing to an image asset
+     */
     setThumbnail(url) {
-        this.thumbnail = { url };
+        if (url.length > 0 && url.length <= 2048) this.thumbnail = { url };
+        else throw new RangeError("[MessageEmbed.setThumbnail] 'url' must be a well-formed URL longer than 0 and shorter or equal to 2048 characters");
 
         return this;
     }
 
+    /**
+     * @param {String} url An url pointing to an image asset
+     */
     setImage(url) {
-        this.image = { url };
+        if (url.length > 0 && url.length <= 2048) this.image = { url };
+        else throw new RangeError("[MessageEmbed.setImage] 'url' must be a well-formed URL longer than 0 and shorter or equal to 2048 characters");
 
         return this;
     }
 
-    setTimestamp(time = new Date()) {
+    /**
+     * @param {DateConstructor} [time]
+     */
+    setTimestamp(time = Date.now()) {
+        if (time instanceof Date) time = time.getTime();
         this.timestamp = time;
 
         return this;
     }
 
-    setFooter(text, icon_url) {
-        this.footer = { text: text.toString().slice(2048), icon_url };
+    /**
+     * @param {String} text
+     * @param {String} [iconUrl]
+     */
+    setFooter(text, iconUrl) {
+        const tempFooter = {};
+
+        if (this.options.truncate || this.options.preventErrors) {
+            tempFooter.text = truncate(text, 2048, this.options.truncate || "");
+            if (iconUrl) tempFooter.icon_url = truncate(icon_url, 2048, this.options.truncate || "");
+        } else {
+            if (text.length > 0 && text.length <= 2048) tempFooter.text = text;
+            else throw new RangeError("[MessageEmbed.setFooter] 'text' must be a string longer than 0 characters and shorter than or equal to 2048 characters");
+
+            if (iconUrl) {
+                if (iconUrl.length > 0 && iconUrl.length <= 2048) tempFooter.icon_url = iconUrl;
+                else throw new RangeError("[MessageEmbed.setFooter] 'iconUrl' must be a string longer than 0 characters and shorter than or equal to 2048 characters");
+            }
+        }
 
         return this;
     }
 
+    /**
+     * @param {String} name
+     * @param {String} value
+     * @param {Boolean} [inline=true]
+     */
     addField(name, value, inline = false) {
         if (this.fields.length >= 25) {
-            console.warn("Limit of 25 fields exceeded, no field was added");
-            return this;
-        } else if (!name || !value) {
-            console.warn("Both a name & value are needed, no field was addded");
-            return this;
+            if (this.options.preventErrors) {
+                console.warn("[MessageEmbed.addField] A max of 25 fields are allowed, current field couldn't be added");
+                return this;
+            } else {
+                throw new Error("[MessageEmbed.addField] A max of 25 fields are allowed, current field would exceed limit");
+            }
         }
 
-        this.fields.push({ name: toString().slice(256), value: value.toString().slice(256), inline });
+        if (!name || !value && !this.options.preventErrors) throw new Error("[MessageEmbed.addField] both 'name' & 'value' are required");
+
+        const tempField = { name: "Not set", value: "Not set", inline };
+
+        if (this.options.truncate || this.options.preventErrors) {
+            tempField.name = truncate(name, 256, this.options.truncate || "");
+            tempField.value = truncate(value, 1024, this.options.truncate || "");
+        } else {
+            if (name.length > 0 && name.length <= 256) tempField.name = name;
+            else throw new RangeError("[MessageEmbed.addField] 'name' must be a string longer than 0 characters and shorter than or equal 256 characters");
+
+            if (value.length > 0 && value.length <= 1048) tempField.value = value;
+            else throw new RangeError("[MessageEmbed.addField] 'value' must be a string longer than 0 characters and shorter than or equal 1048 characters");
+        }
+
+        this.fields.push(tempField);
+
+        return this;
     }
 
+    /**
+     * @description Clears all fields
+     */
     clearFields() {
         this.fields = [];
+
+        return this;
     }
 }
 
 module.exports.init = Eris => {
-    Eris.MessageEmbed = MessageEmbed;
+    addImport(Eris, MessageEmbed);
 };
