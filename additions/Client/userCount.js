74339491cb6a2634377add173b6e267953fd2a4f
Exhabition <lukeweerheim@hotmail.com>
2021-09-21 20:23:40 +0200

    Tested userCount and fixed issues

--- additions/Client/userCount.js
+++ additions/Client/userCount.js
@@ -5,6 +5,6 @@ const { addPrototype } = require("erisify/helper/prototype");
  */
 module.exports.init = (Eris) => {
     addPrototype(Eris, "Client", function userCount() {
-        return this.guilds?.length > 0 ? this.guilds.map(guild => guild.memberCount).reduce((total, memberCount) => total + memberCount, 0) : 0;
+        return this.guilds?.size > 0 ? this.guilds.map(guild => guild.memberCount).reduce((total, memberCount) => total + memberCount, 0) : 0;
     }, true);
 };
